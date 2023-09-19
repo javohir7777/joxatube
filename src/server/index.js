01dbb1d8c38fdc32b761a46f1cb3279be99aed1f
javohir7777 <javohirjumayev7777@gmail.com>
2023-09-19 00:08:38 -0700

    2-chi account

--- src/server/index.js
+++ src/server/index.js
@@ -11,7 +11,7 @@ const options = {
     order: "date",
   },
   headers: {
-    "X-RapidAPI-Key": "a3334b51aamsh7c960ce71450d90p1c6052jsn0a269bfcb1df",
+    "X-RapidAPI-Key": "f399c28c01mshbc2264a15764dcbp112910jsn961c77f286b9",
     "X-RapidAPI-Host": "youtube-v31.p.rapidapi.com",
   },
 };
